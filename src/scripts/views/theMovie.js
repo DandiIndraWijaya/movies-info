0a23a1ade5e6fc01899e3ca788e45492493621ac
DandiIndraWijaya <dandiindra29@gmail.com>
2020-08-04 11:32:54 +0700

    second commit

---
+++ src/scripts/views/theMovie.js
@@ -0,0 +1,42 @@
+
+// Menampilkan movie hasil fetch data
+const theMovie =  movies => {
+    let theMovie = ``;
+    movies.forEach(movie => {
+        const release_date = new Date(movie.release_date)
+        theMovie += `
+        <div class="col-6 col-sm-6 col-md-3">
+                <div class="card the-movie">
+                    <img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2${movie.poster_path}">
+                    <h6>${movie.title} </h6>
+                    <div class="rating"><span class="vote">${movie.vote_average * 10}</span><sup>%</sup></div>
+                    <p>${release_date.toDateString()}</p>
+                    <div class="movie-button"><button class="detail">Detail</button> <button class="trailer"><i class="fa fa-lg fa-facebook" style="width: 20px" aria-hidden="true"></i>Trailer</button></div>
+                </div>
+        </div>`;
+    });
+
+    let moviesContainer = document.querySelector('.movies-container');
+    moviesContainer.innerHTML = theMovie;
+
+    // Memberi warna rating
+    const getTheMovie = document.querySelectorAll('.rating');
+    const getVote = document.querySelectorAll('.vote');
+    for(let i = 0 ; i < getTheMovie.length ; i++){
+        console.log(getVote[i].textContent);
+        if(getVote[i].textContent <= 50){
+            getTheMovie[i].classList.add('low-rate')
+        }else if(getVote[i].textContent > 50 && getVote[i].textContent < 70){
+            getTheMovie[i].classList.add('med-rate')
+        }else{
+            getTheMovie[i].classList.add('high-rate');
+        }
+    }
+
+    // Memformat date
+    
+}
+
+export {
+    theMovie
+}
